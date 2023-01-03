fbf3964b3526e979920131e850ec5a3a1289b105
Mason Guaman <masonstalin@gmail.com>
2023-01-03 13:05:27 -0500

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by 5e on 1/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

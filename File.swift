13ea58970d2ad2dfa9ed9244b3cbafb66c44c8c0
Mason Guaman <masonstalin@gmail.com>
2023-01-03 13:51:50 -0500

    ty

---
+++ File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  Emoji Dictionary
+//
+//  Created by 5e on 1/3/23.
+//
+
+import Foundation
+
+
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
